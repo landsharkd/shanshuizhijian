3af1dc11dc7427d88b4a1d960a4ea02fcdf3a8d7
lei.du <lei.du@phicomm.com>
2018-07-11 16:46:57 +0800

    init

---
+++ src/main/java/com/sszj/shanshuizhijian/ShanshuizhijianApplication.java
@@ -0,0 +1,12 @@
+package com.sszj.shanshuizhijian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanshuizhijianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShanshuizhijianApplication.class, args);
+	}
+}
